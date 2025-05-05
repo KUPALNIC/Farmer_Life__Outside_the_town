05b9a5bde068dcaacbc9c6bf7a90a840b98bc55a
KUPALNIC <sashakupa2411@gmail.com>
2025-05-06 02:52:11 +0300

    some changes

--- src/Game/World.cpp
+++ src/Game/World.cpp
@@ -1,18 +1,60 @@
 #include "World.hpp"
 
 World::World() {
-    // Инициализация мира
-    for (int y = 0; y < worldSize/2; ++y) {
-        for (int x = 0; x < worldSize/2; ++x) {
-            worldGrid[y][x] = Biome::PLAINS;
-            worldGrid[y+worldSize/2][x] = Biome::FOREST;
-            worldGrid[y][x+worldSize/2] = Biome::SWAMP;
-            worldGrid[y+worldSize/2][x+worldSize/2] = Biome::MOUNTAINS;
+    // Генерация карты
+    int halfSize = worldSize / 2; // Размер половины карты
+
+    // Равнины (левый верхний угол)
+    generateBiome(0, 0, halfSize, halfSize, Biome::PLAINS);
+
+    // Болото (нижний левый угол)
+    generateBiome(0, halfSize, halfSize, halfSize, Biome::SWAMP);
+
+    // Лес (нижний правый угол)
+    generateBiome(halfSize, halfSize, halfSize, halfSize, Biome::FOREST);
+
+    // Горы (верхний правый угол)
+    generateBiome(halfSize, 0, halfSize, halfSize, Biome::MOUNTAINS);
+
+    // Настройка клеток
+    cell.setSize(sf::Vector2f(static_cast<float>(cellSize), static_cast<float>(cellSize)));
+}
+
+void World::generateBiome(int startX, int startY, int width, int height, Biome biome) {
+    std::random_device rd;
+    std::mt19937 rng(rd());
+
+    for (int y = startY; y < startY + height; ++y) {
+        for (int x = startX; x < startX + width; ++x) {
+            worldGrid[y][x] = biome;
+            colorGrid[y][x] = getRandomBiomeColor(biome); // Генерируем цвет для клетки
+
+            // TODO: Заменить на спрайты
+            // Здесь вы можете загрузить текстуру для текущего биома и присвоить её клетке.
+            // Пример:
+            // if (biome == Biome::PLAINS) {
+            //     cell.setTexture(&plainsTexture);
+            // }
         }
     }
+}
 
-    cell.setSize(sf::Vector2f(32.0f, 32.0f));
-    cell.setFillColor(sf::Color::Green);
+sf::Color World::getRandomBiomeColor(Biome biome) {
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> variation(0, 50); // Для небольшого изменения цвета
+
+    switch (biome) {
+    case Biome::PLAINS: // Светлые оттенки зеленого
+        return sf::Color(150 + variation(rng), 255, 150 + variation(rng));
+    case Biome::FOREST: // Темные оттенки зеленого
+        return sf::Color(0, 100 + variation(rng), 0);
+    case Biome::SWAMP: // Коричневые с зеленым
+        return sf::Color(100 + variation(rng), 70 + variation(rng), 50);
+    case Biome::MOUNTAINS: // Оранжевые с серым
+        return sf::Color(200 + variation(rng), 150 + variation(rng), 100 + variation(rng));
+    }
+    return sf::Color::White; // Дефолтный цвет
 }
 
 void World::update(float deltaTime) {
@@ -22,7 +64,12 @@ void World::update(float deltaTime) {
 void World::render(sf::RenderWindow& window) {
     for (int y = 0; y < worldSize; ++y) {
         for (int x = 0; x < worldSize; ++x) {
-            cell.setPosition({x * 32.0f, y * 32.0f});
+            cell.setPosition({static_cast<float>(x * cellSize), static_cast<float>(y * cellSize)});
+            cell.setFillColor(colorGrid[y][x]); // Устанавливаем цвет клетки из заранее сгенерированного массива
+
+            // TODO: Заменить на спрайты
+            // Здесь вы можете установить текстуру вместо цвета:
+            // cell.setTexture(&yourTexture);
             window.draw(cell);
         }
     }
--- src/Game/World.hpp
+++ src/Game/World.hpp
@@ -1,9 +1,10 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <array>
+#include <random>
 
 enum class Biome { PLAINS, FOREST, SWAMP, MOUNTAINS };
-// написать класс biom у которго будет функция распределения клеток, из трех или четырех различных спрайтов, а от него дочерние классы
+
 class World {
 public:
     World();
@@ -12,9 +13,19 @@ public:
     void render(sf::RenderWindow& window);
 
 private:
-    const int worldSize = 150;
-    std::array<std::array<Biome, 150>, 150> worldGrid;
+    const int worldSize = 150; // Размер карты (в клетках)
+    const int cellSize = 32;   // Размер клетки (в пикселях)
 
-    // sf::RectangleShape cell; // Временный спрайт клетки
+    std::array<std::array<Biome, 150>, 150> worldGrid;  // Матрица биомов
+    std::array<std::array<sf::Color, 150>, 150> colorGrid; // Цвета клеток биомов
+    sf::RectangleShape cell;                            // Временный спрайт клетки
 
+    // Генерация биома для определенной области
+    void generateBiome(int startX, int startY, int width, int height, Biome biome);
+
+    // Получение случайного цвета для конкретного биома
+    sf::Color getRandomBiomeColor(Biome biome);
+
+    // TODO: Заменить квадраты на спрайты
+    // Вы можете добавить сюда std::map<Biome, sf::Texture> для хранения текстур.
 };
